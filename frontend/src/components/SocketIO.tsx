c80ed120cfba262a1c086df3e2f03e58bcd78306
aronmal <aron@mal-noh.de>
2022-10-28 22:21:55 +0200

    Add functionality to Socket.IO client

--- frontend/src/components/SocketIO.tsx
+++ frontend/src/components/SocketIO.tsx
@@ -1,8 +1,27 @@
 import { io } from 'socket.io-client';
 
 function SocketIO() {
-    // const socket = io("wss://server-domain.com");
-    const socket = io(); // The server URL will be deduced from the window.location object.
+    const socket = io("ws://localhost:5001");
+    socket.on('test2', (warst) => {
+        console.log('Test2:', warst, socket.id)
+    })
+    socket.on("connect", () => {
+        console.log(socket.connected); // true
+        setTimeout(() => {
+            socket.emit('test', "warst")
+            socket.emit('test', "tsra")
+            socket.emit('test', "1234")
+            // socket.disconnect()
+        }, 1000)
+    });
+
+    socket.on("test", () => {
+        console.log("Got test1234"); // false
+    });
+
+    socket.on("disconnect", () => {
+        console.log(socket.connected); // false
+    });
     return (
         <div>SocketIO</div>
     )
